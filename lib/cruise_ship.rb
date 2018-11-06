e1477ff91c7093543d007a83aef14cb7da904790
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-06 03:02:02 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,9 +10,7 @@ def select_winner(passengers)
   winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
-  winner = name
+  return winner = name
   end
 end
 end
-
-winner
